f127c09497553e9fc88eb546eb1f3db1fa8e2479
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-15 21:37:53 -0400

    useContext situação real

---
+++ src/components/Div/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { H1 } from '../H1';
+import { P } from '../P';
+
+export const Div = () => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  );
+};
