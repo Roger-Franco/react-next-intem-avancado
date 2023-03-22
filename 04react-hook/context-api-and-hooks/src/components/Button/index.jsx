d18d5bd6a4dadc8ef676530cf423d419341047d4
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-22 17:48:21 -0400

    final context API + Hooks

--- 04react-hook/context-api-and-hooks/src/components/Button/index.jsx
+++ 04react-hook/context-api-and-hooks/src/components/Button/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import P from 'prop-types';
 import { useCounterContext } from '../../contexts/CounterContext';
 
-export const Button = ({ children, onButtonClick }) => {
-  return <button onClick={onButtonClick}>{children}</button>;
+export const Button = ({ children, onButtonClick, disabled = false }) => {
+  return (
+    <button disabled={disabled} onClick={onButtonClick}>
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
   children: P.node.isRequired,
   onButtonClick: P.func.isRequired,
+  disabled: P.bool.isRequired,
 };
--- 04react-hook/context-api-and-hooks/src/templates/Home/index.jsx
+++ 04react-hook/context-api-and-hooks/src/templates/Home/index.jsx
@@ -7,15 +7,41 @@ export const Home = () => {
   const [state, actions] = useCounterContext();
   // console.log(state);
   // console.log(actions);
-  useEffect(() => {
-    actions.increase();
-  }, [actions]);
+  // useEffect(() => {
+  //   actions.increase();
+  // }, [actions]);
+
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message));
+  };
 
   return (
     <div>
       <Heading />
       <div>
         <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.decrease}>Decrease</Button>
+        <Button onButtonClick={actions.reset}>Reset</Button>
+        <Button
+          onButtonClick={
+            () => actions.setCounter({ counter: 10 })
+            // actions.setCounter({ counter: state.counter + 10 })
+          }
+        >
+          set 10
+        </Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>
+          set 100
+        </Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          async increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          async Error
+        </Button>
       </div>
     </div>
   );
