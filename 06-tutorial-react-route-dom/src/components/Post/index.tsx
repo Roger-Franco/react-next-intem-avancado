5189be0b3998326d36290f86ff4f054b03871d32
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-23 09:11:32 -0400

    useParams e useSearchParams

---
+++ 06-tutorial-react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,18 @@
+import { useParams, useSearchParams } from 'react-router-dom'
+import './style.css'
+
+export const Post = () => {
+  const params = useParams()
+  const {id} = params
+  const [qs, setqs] = useSearchParams()
+  console.log(params);
+  // http://127.0.0.1:5173/posts/10?page=120
+  
+  return (
+    <div>
+      <h1>Post {`Param: ${id}`} {`Query String: ${qs.get('page')}`}</h1>
+      <h1>Post {`Param: ${id}`} {`Query String: ${qs.get('segundo')}`}</h1>
+      <h1>Post {`Param: ${id}`} {`Query String: ${qs}`}</h1>
+    </div>
+  )
+}
